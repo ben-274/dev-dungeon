405885df85f586caa67162f5c0d11da0464a3117
ben-274 <ben-274@gmx.de>
2025-06-20 09:56:22 +0200

    Refactoring IllusionRiddleLevel: Extract Method spawnSecretPassages

--- devDungeon/src/level/devlevel/IllusionRiddleLevel.java
+++ devDungeon/src/level/devlevel/IllusionRiddleLevel.java
@@ -162,16 +162,7 @@ public class IllusionRiddleLevel extends DevDungeonLevel implements ITickable {
       spawnBossAndHandleEvents();
 
       // Secret Passages
-      EntityUtils.spawnLever(
-          this.leverSpawns[0].toCenteredPoint(),
-          new OpenPassageCommand(this.secretPassages[0][0], this.secretPassages[0][1]));
-      EntityUtils.spawnLever(
-          this.leverSpawns[1].toCenteredPoint(),
-          new OpenPassageCommand(this.secretPassages[1][0], this.secretPassages[1][1]));
-      EntityUtils.spawnLever(
-          this.leverSpawns[2].toCenteredPoint(),
-          new OpenPassageCommand(this.secretPassages[2][0], this.secretPassages[2][1]));
-      this.spawnChestsAndCauldrons();
+      spawnSecretPassages();
     }
 
     if (this.lastRoom != this.getCurrentRoom()) {
@@ -212,6 +203,19 @@ public class IllusionRiddleLevel extends DevDungeonLevel implements ITickable {
     }
 
     this.riddleHandler.onTick(isFirstTick);
+  }
+
+  private void spawnSecretPassages() {
+	EntityUtils.spawnLever(
+          this.leverSpawns[0].toCenteredPoint(),
+          new OpenPassageCommand(this.secretPassages[0][0], this.secretPassages[0][1]));
+      EntityUtils.spawnLever(
+          this.leverSpawns[1].toCenteredPoint(),
+          new OpenPassageCommand(this.secretPassages[1][0], this.secretPassages[1][1]));
+      EntityUtils.spawnLever(
+          this.leverSpawns[2].toCenteredPoint(),
+          new OpenPassageCommand(this.secretPassages[2][0], this.secretPassages[2][1]));
+      this.spawnChestsAndCauldrons();
   }
 
   private void spawnBossAndHandleEvents() {
