99cc826541e614dcd770ccf45c3d2de5dd986161
ben-274 <ben-274@gmx.de>
2025-06-20 09:08:42 +0200

    Refactoring IllusionRiddleLevel: Extract Method initializeLevel

--- devDungeon/src/level/devlevel/IllusionRiddleLevel.java
+++ devDungeon/src/level/devlevel/IllusionRiddleLevel.java
@@ -151,19 +151,7 @@ public class IllusionRiddleLevel extends DevDungeonLevel implements ITickable {
   @Override
   public void onTick(boolean isFirstTick) {
     if (isFirstTick) {
-      DialogFactory.showTextPopup(
-          "Wait, who turned off the lights? Try to find a way out of this dark place.",
-          "Level " + DevDungeon.DUNGEON_LOADER.currentLevelIndex() + ": The Illusion Riddle");
-
-      ((ExitTile) this.endTile()).close(); // close exit at start (to force defeating the boss)
-      this.doorTiles().forEach(DoorTile::close);
-      this.pitTiles()
-          .forEach(
-              pit -> {
-                pit.timeToOpen(50L * Game.currentLevel().RANDOM.nextInt(1, 5));
-                pit.close();
-              });
-      this.rooms.forEach(DevDungeonRoom::spawnEntities);
+      initializeLevel();
 
       // Create teleporters
       for (int i = 65; i < 127; i += 2) {
@@ -289,6 +277,28 @@ public class IllusionRiddleLevel extends DevDungeonLevel implements ITickable {
     }
 
     this.riddleHandler.onTick(isFirstTick);
+  }
+
+  /**
+   * Initializes the Level
+   * 
+   * <p>This Method initializes the start dialog that is shown in the beginning.
+   * The exit to the next Level is closed to defead the Boss. The Method also spaws all the enemies.</p>
+   */
+  private void initializeLevel() {
+	DialogFactory.showTextPopup(
+          "Wait, who turned off the lights? Try to find a way out of this dark place.",
+          "Level " + DevDungeon.DUNGEON_LOADER.currentLevelIndex() + ": The Illusion Riddle");
+
+      ((ExitTile) this.endTile()).close(); // close exit at start (to force defeating the boss)
+      this.doorTiles().forEach(DoorTile::close);
+      this.pitTiles()
+          .forEach(
+              pit -> {
+                pit.timeToOpen(50L * Game.currentLevel().RANDOM.nextInt(1, 5));
+                pit.close();
+              });
+      this.rooms.forEach(DevDungeonRoom::spawnEntities);
   }
 
   /** TODO: Refactor this method, and add JavaDoc */
